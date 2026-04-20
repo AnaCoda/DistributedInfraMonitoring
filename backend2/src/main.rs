7cd7d718afaf5618ed5e5016df6ed9c648e7f0c6
Homer Riva-Cambrin <neithercouldterry@gmail.com>
2026-04-20 00:17:33 -0600

    test

--- backend2/src/main.rs
+++ backend2/src/main.rs
@@ -1,8 +1,47 @@
-use std::{thread, time::Duration};
+use axum::{
+    extract::ws::{Message, WebSocket, WebSocketUpgrade},
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+use futures::{SinkExt, StreamExt};
+use std::net::SocketAddr;
 
-fn main() {
-    loop {
-        println!("Hello world!");
-        thread::sleep(Duration::from_secs(1));
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/ws", get(ws_handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn ws_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
+    ws.on_upgrade(handle_socket)
+}
+
+async fn handle_socket(mut socket: WebSocket) {
+    println!("Client connected");
+
+    while let Some(Ok(msg)) = socket.recv().await {
+        match msg {
+            Message::Text(text) => {
+                println!("Received: {}", text);
+
+                // echo back
+                if socket.send(Message::Text(text)).await.is_err() {
+                    break;
+                }
+            }
+            Message::Close(_) => {
+                println!("Client disconnected");
+                break;
+            }
+            _ => {}
+        }
     }
 }
