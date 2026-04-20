d0c9fdd0b7820a669148ee9d437fb01ea3e1dc41
Homer Riva-Cambrin <neithercouldterry@gmail.com>
2026-04-19 23:25:41 -0600

    replication tracking

--- backend/common/components/plugins/replication/replication_plugin.py
+++ backend/common/components/plugins/replication/replication_plugin.py
@@ -180,6 +180,7 @@ class ReplicationPlugin(Plugin):
                 except Exception as e:
                     print(f'{Fore.RED}[{self.get_network_name()}] Failed to send to {replica} with error={e}{Fore.RESET}')
                     pass
+                print(f'[{self.get_network_name()}] Succesfully replicated option to {body}')
                     # print(f'excepted {type(e)}')
             return output
         else:
